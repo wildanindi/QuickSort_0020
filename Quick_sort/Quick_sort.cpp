2639a8c1965fc5328b775ece3b520aeacc0ea192
wildanindi <wildanindi2@gmail.com>
2024-03-28 17:30:13 +0700

    menambah fungsi while untuk mencari elemen yang lebih besar dari pivot

--- Quick_sort/Quick_sort.cpp
+++ Quick_sort/Quick_sort.cpp
@@ -51,6 +51,15 @@ void q_short(int low, int high)
     pivot = arr[low];
     i = low + 1;
     j = high;
+
+    while (i <= j)
+    {
+        while ((arr[i] <= pivot) && (i <= high))
+        {
+            i++;
+            cmp_count++;
+        }
+    }
 }
 
 int main()
