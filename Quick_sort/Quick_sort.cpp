8f0d778e462b2c22554368449283b7ee941f6521
wildanindi <wildanindi2@gmail.com>
2024-03-28 17:41:55 +0700

    menambahkan fungsi for untuk melalukan perulangan pada array

--- Quick_sort/Quick_sort.cpp
+++ Quick_sort/Quick_sort.cpp
@@ -92,6 +92,11 @@ void display() {
     cout << "\n---------------------" << endl;
     cout << "Sorted Array" << endl;
     cout << "---------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+
+    }
 }
 
 int main()
