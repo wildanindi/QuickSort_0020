4bf5a7452f3f05d68f2f669f798ea851af6db4c1
wildanindi <wildanindi2@gmail.com>
2024-03-28 17:52:28 +0700

    menambahkan fungsi do untuk melakukan pengulangan, dengan catatan fungsi tsb berjalan jika user memilih pilihan untuk melakukan perulangan

--- Quick_sort/Quick_sort.cpp
+++ Quick_sort/Quick_sort.cpp
@@ -110,8 +110,14 @@ int main()
         input();
         q_short(0, n - 1);
         display();
+        cout << "\n\nDo you want to continue? (y/n:";
+        cin >> ch;
+
+        if (ch == 'n' || ch == 'N')
+            break;
+
         
-    }
+    } while (true);
     
 }
 
