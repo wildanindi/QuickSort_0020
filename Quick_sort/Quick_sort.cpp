baaf9c8311a715fa77af62466841466686077532
wildanindi <wildanindi2@gmail.com>
2024-03-28 17:16:15 +0700

    menamhkan prosedur input untuk memasukkan element dari array

--- Quick_sort/Quick_sort.cpp
+++ Quick_sort/Quick_sort.cpp
@@ -6,7 +6,23 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukkan Panjang Element Array : ";
+        cin >> n;
 
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "\n--------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n--------------------" << endl;
+
+}
 
 
 int main()
